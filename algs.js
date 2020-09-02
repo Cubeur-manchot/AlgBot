1ecf27a0f9202e0ba1102b2cd8791e9e54ce53cd
Cubeur-manchot <matthieu.aubert17@gmail.com>
2020-09-02 22:11:29 +0200

    Add OLL collection

--- algs.js
+++ algs.js
@@ -25,6 +25,65 @@ const algCollection = {
 		pll_y: "F R U' R' U' R U R' F' R U R' U' R' F R F'",
 		pll_z: "M2' U M2' U M' U2 M2' U2 M'"
 	},
+	OLLCollection: {
+		oll_1: "R U2' R2' F R F' U2' R' F R F'",
+		oll_2: "F R U R' U' S R U R' U' f'",
+		oll_3: "f R U R' U' f' U' F R U R' U' F'",
+		oll_4: "f R U R' U' f' U F R U R' U' F'",
+		oll_5: "r' U2' R U R' U r",
+		oll_6: "r U2' R' U' R U' r'",
+		oll_7: "r U R' U R U2' r'",
+		oll_8: "r' U' R U' R' U2' r",
+		oll_9: "R U R' U' R' F R2 U R' U' F'",
+		oll_10: "R U R' U R' F R F' R U2' R'",
+		oll_11: "r' R2 U R' U R U2' R' U M'",
+		oll_12: "r R2' U' R U' R' U2' R U' r' R",
+		oll_13: "F U R U2 R' U' R U R' F'",
+		oll_14: "R' F R U R' F' R F U' F'",
+		oll_15: "r' U' r R' U' R U r' U r",
+		oll_16: "r U r' R U R' U' r U' r'",
+		oll_17: "R U R' U R' F R F' U2' R' F R F'",
+		oll_18: "r U R' U R U2' r2' U' R U' R' U2' r",
+		oll_19: "r' U2' R U R' U r2 U2' R' U' R U' r'",
+		oll_20: "r U R' U' M2' U R U' R' U' M'",
+		oll_21: "R U2 R' U' R U R' U' R U' R'",
+		oll_22: "R U2' R2' U' R2 U' R2' U2' R",
+		oll_23: "R2' D' R U2 R' D R U2 R",
+		oll_24: "r U R' U' r' F R F'",
+		oll_25: "F' r U R' U' r' F R",
+		oll_26: "R U2' R' U' R U' R'",
+		oll_27: "R U R' U R U2' R'",
+		oll_28: "r U R' U' M U R U' R'",
+		oll_29: "r2' D' r U r' D r2 U' r' U' r",
+		oll_30: "F U R U2' R' U' R U2 R' U' F'",
+		oll_31: "R' U' F U R U' R' F' R",
+		oll_32: "S R U R' U' R' F R f'",
+		oll_33: "R U R' U' R' F R F'",
+		oll_34: "R U R' U' B' R' F R F' B",
+		oll_35: "R U2' R2' F R F' R U2' R'",
+		oll_36: "L' U' L U' L' U L U L F' L' F",
+		oll_37: "F R U' R' U' R U R' F'",
+		oll_38: "R U R' U R U' R' U' R' F R F'",
+		oll_39: "L F' L' U' L U F U' L'",
+		oll_40: "R' F R U R' U' F' U R",
+		oll_41: "R U R' U R U2' R' F R U R' U' F'",
+		oll_42: "R' U' R U' R' U2' R F R U R' U' F'",
+		oll_43: "R' U' F' U F R",
+		oll_44: "F U R U' R' F'",
+		oll_45: "F R U R' U' F'",
+		oll_46: "R' U' R' F R F' U R",
+		oll_47: "F' L' U' L U L' U' L U F",
+		oll_48: "F R U R' U' R U R' U' F'",
+		oll_49: "r U' r2' U r2 U r2' U' r",
+		oll_50: "r' U r2 U' r2' U' r2 U r'",
+		oll_51: "F U R U' R' U R U' R' F'",
+		oll_52: "R' U' R U' R' U F' U F R",
+		oll_53: "r' U2' R U R' U' R U R' U r",
+		oll_54: "r U2' R' U' R U R' U' R U' r'",
+		oll_55: "R U2' R2' U' R U' R' U2' F R F'",
+		oll_56: "r U r' U R U' R' U R U' R' r U' r'",
+		oll_57: "R U R' U' M' U R U' r'"
+	},
 	basicAlgsCollection: {
 		sune: "R U R' U R U2' R'",
 		antisune: "R U2 R' U' R U' R'",
@@ -228,6 +287,9 @@ const deployMove = move => {
 	} else if (moveLower.includes("pll_")) { // move is actually a PLL
 		moveSequence.movesForAnswer = algCollection.PLLCollection[moveLower];
 		moveSequence.movesForVisualCube = algCollection.PLLCollection[moveLower];
+	} else if (moveLower.includes("oll_")) { // move is actually an OLL
+		moveSequence.movesForAnswer = algCollection.OLLCollection[moveLower];
+		moveSequence.movesForVisualCube = algCollection.OLLCollection[moveLower];
 	} else if (moveLower.includes("sune") || moveLower.includes("niklas")) { // move is a basic alg
 		moveSequence.movesForAnswer = algCollection.basicAlgsCollection[moveLower];
 		moveSequence.movesForVisualCube = algCollection.basicAlgsCollection[moveLower];
@@ -283,6 +345,8 @@ const getAlgListHelpMessage = language => {
 			+ "\nToutes les PLL : `PLL_Aa`, `PLL_Ab`, `PLL_E`, `PLL_F`, `PLL_Ga`, `PLL_Gb`, `PLL_Gc`, `PLL_Gd`, `PLL_H`, `PLL_Ja`, `PLL_Jb`, "
 			+ "`PLL_Na`, `PLL_Nb`, `PLL_Ra`, `PLL_Rb`, `PLL_T`, `PLL_Ua`, `PLL_Ub`, `PLL_V`, `PLL_Y`, `PLL_Z` :"
 			+ "```parser3\n$alg R' PLL_Y R```"
+			+ "\nToutes les  OLL : `OLL_1`, `PLL_2`, ..., `OLL_57` :"
+			+ "```parser3\n$alg OLL_37 -oll```"
 			+ "\nLes sunes, antisunes et composés, les niklas :"
 			+ "```parser3\n$alg tripleantisune niklasright```"
 			+ "\nLes parités du <:4x4x4:693863938915565599> :"
@@ -296,6 +360,8 @@ const getAlgListHelpMessage = language => {
 			+ "\nAll the PLL : `PLL_Aa`, `PLL_Ab`, `PLL_E`, `PLL_F`, `PLL_Ga`, `PLL_Gb`, `PLL_Gc`, `PLL_Gd`, `PLL_H`, `PLL_Ja`, `PLL_Jb`, "
 			+ "`PLL_Na`, `PLL_Nb`, `PLL_Ra`, `PLL_Rb`, `PLL_T`, `PLL_Ua`, `PLL_Ub`, `PLL_V`, `PLL_Y`, `PLL_Z` :"
 			+ "```parser3\n$alg R' PLL_Y R```"
+			+ "\nAll the OLL : `OLL_1`, `PLL_2`, ..., `OLL_57` :"
+			+ "```parser3\n$alg OLL_37 -oll```"
 			+ "\nSunes, antisunes and compositions of them, niklas :"
 			+ "```parser3\n$alg tripleantisune niklasright```"
 			+ "\n<:4x4x4:693863938915565599> parities :"
