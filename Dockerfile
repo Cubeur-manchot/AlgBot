a155f02b638e87e27ca5901d7d2697b7d627c56f
Cubeur-manchot <matthieu.aubert17@gmail.com>
2020-12-08 08:45:20 +0100

    Dockerization

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:14
+
+ENV NODE_ENV production
+
+COPY package.json .
+
+RUN npm install
+
+COPY index.js eventHandler.js messageHandler.js commandHandler.js help.js options.js algs.js algCollection.js merging.js /
+
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-/* console commands :
-- initialize npm : npm init
-- install discord.js : npm install --save discord.js
-- install dotenv.js : npm install --save dotenv
-- install pm2.js : npm install --save pm2
-- install pm2-windows-startup : npm install --save pm2-windows-startup
-- launch script via node : node index.js
-- or launch script via pm2 : pm2 start index.js
-*/
+"use strict";
 
 const Discord = require("discord.js");
-require("dotenv").config();
 
 const {onReady, onMessage, onMessageUpdate, onMessageDelete} = require("./eventHandler.js");
 
