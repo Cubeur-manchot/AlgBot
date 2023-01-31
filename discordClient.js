9ae35d3124f527daf65988e5f3d2a12e59d76a82
Cubeur-manchot <matthieu.aubert17@gmail.com>
2023-01-31 14:08:59 +0100

    Create a Discord client and log in

--- discordClient.js
+++ discordClient.js
@@ -1,9 +1,18 @@
 "use strict";
 
+const Discord = require("discord.js");
+
 class DiscordClient {
 	constructor(algBot) {
 		this.algBot = algBot;
-		// todo
+		this.client = new Discord.Client();
+		this.login();
+	};
+	login = () => {
+		let language = this.algBot.language;
+		this.client.login(process.env[`TOKEN_${language.toUpperCase()}`])
+			.then(() => console.log(`AlgBot (${language}) is logged in !`))
+			.catch(() => console.error(`Login error with AlgBot (${language}).`));
 	};
 };
 
