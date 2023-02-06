65dacc2167c4ca1e5f586d0d3c21152c3eb0bf2e
Cubeur-manchot <matthieu.aubert17@gmail.com>
2023-02-07 00:05:34 +0100

    Delete result of command when it the command is deleted

--- messageHandler.js
+++ messageHandler.js
@@ -3,11 +3,25 @@
 class MessageHandler {
 	constructor(algBot) {
 		this.algBot = algBot;
-		// todo
 	};
 	onMessageCreate = message => {
 	};
 	onMessageDelete = message => {
+		if (this.messageIsAlgBotCommand(message)) {
+			this.algBot.discordClient.deleteMessage(this.findAlgBotAnswer(message));
+		}
+	};
+	messageIsAlgBotCommand = message => {
+		return message.content.startsWith(this.algBot.prefix)
+			&& !message.author.bot;
+	};
+	messageIsAlgBotMessage = message => {
+		return message.author.id === this.algBot.discordClient.user.id;
+	};
+	findAlgBotAnswer = message => {
+		return [...message.channel.messages.cache.values()].find(channelMessage =>
+			this.messageIsAlgBotMessage(channelMessage) && channelMessage.reference.messageId === message.id
+		);
 	};
 };
 
