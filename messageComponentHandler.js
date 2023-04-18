334bf98ee3de7950f3bcf00e55913943a15488de
Cubeur-manchot <matthieu.aubert17@gmail.com>
2023-04-19 01:14:35 +0200

    Remove useless methods

--- messageComponentHandler.js
+++ messageComponentHandler.js
@@ -3,20 +3,6 @@
 import Discord from "discord.js";
 
 class MessageComponentHandler {
-	static createRow = components => {
-		return {
-			type: Discord.ComponentType.ActionRow,
-			components: components
-		};
-	};
-	static createLinkButton = (label, url) => {
-		return {
-			type: Discord.ComponentType.Button,
-			style: Discord.ButtonStyle.Link,
-			label: label,
-			url: url
-		};
-	};
 	static createRowWithSelectComponents = (selectOptions, selectedOption, customId) => {
 		selectOptions.forEach(selectOption => selectOption.default = selectOption.value === selectedOption);
 		return [MessageComponentHandler.createRow([
