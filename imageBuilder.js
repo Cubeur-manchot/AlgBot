9b3d3085ac57c3dc40113579623fe102e8f20a13
Cubeur-manchot <matthieu.aubert17@gmail.com>
2023-08-25 00:17:29 +0200

    Move cube image size to separate variable

--- imageBuilder.js
+++ imageBuilder.js
@@ -3,11 +3,12 @@
 import {OptionsHandler} from "./optionsHandler.js";
 
 class ImageBuilder {
+	static cubeImageSize = 150;
 	constructor(algCommandHandler) {
 		this.algCommandHandler = algCommandHandler;
 	};
 	buildVisualCubeImage = (moveSequence, optionsObject, cubeSize) => {
-		let urlBegin = "http://cube.rider.biz/visualcube.php?fmt=png&bg=t&size=150";
+		let urlBegin = "http://cube.rider.biz/visualcube.php";
 		let view = optionsObject.view === OptionsHandler.planView ? "&view=plan" : "";
 		let colorScheme = [
 			optionsObject.colorScheme.U,
@@ -27,7 +28,7 @@ class ImageBuilder {
 			.replace(/\s/g, "%20") // replace spaces
 			.replace(/'/g, "%27"); // replace apostrophes
 		return {
-			url: `${urlBegin}${view}&pzl=${cubeSize}&sch=${colorScheme}&stage=${stage}&${caseOrAlg}=${moveSequenceForVisualCube}`,
+			url: `${urlBegin}?fmt=png&size=${ImageBuilder.cubeImageSize}&bg=t${view}&pzl=${cubeSize}&sch=${colorScheme}&stage=${stage}&${caseOrAlg}=${moveSequenceForVisualCube}`,
 			errors: []
 		};
 	};
